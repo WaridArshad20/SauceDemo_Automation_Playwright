d2f99870ed6c856a8526e8c5fcf72880f8dc3ba5
Warid-Klabs <warid@byklabs.com>
2026-05-05 19:41:02 +0500

    Added beforeEach in homepage.spec.js

--- tests/e2e/homepage.spec.js
+++ tests/e2e/homepage.spec.js
@@ -2,15 +2,16 @@ const { test } = require('../../fixtures/base')
 const { expect } = require('@playwright/test');
 const product = require('../../data/productData.json');
 
-test('product_add_to_cart', async ({pm}) => {
-    
+test.beforeEach (async ({pm}) => {
     await pm.homePage.gotoHomePage();
+})
+
+test('product_add_to_cart', async ({pm}) => {
     await pm.homePage.addToCart(product.bikelight.slug)
     await expect(pm.homePage.cartCountLocator).toHaveText('1');
 })
 
 test('sort products by price low to high', async({pm}) => {
-    await pm.homePage.gotoHomePage();
     await pm.homePage.sortBy(product.sortOptions.lowToHigh);
     const productPrices = await pm.homePage.getProductPrices();
     // const sorted = [...productPrices].sort((a,b) => a-b);
@@ -20,7 +21,6 @@ test('sort products by price low to high', async({pm}) => {
 })
 
 test('sort products by price high to low', async ({pm}) => {
-    await pm.homePage.gotoHomePage();
     await pm.homePage.sortBy(product.sortOptions.highToLow);
     const productPrices = await pm.homePage.getProductPrices();
     const sorted = [...productPrices].sort((a,b) => b-a);
@@ -28,7 +28,6 @@ test('sort products by price high to low', async ({pm}) => {
 })
 
 test('sort product by alphabatically a-z', async({ pm, page}) => {
-    await pm.homePage.gotoHomePage();
     await pm.homePage.sortBy(product.sortOptions.nameAtoZ);
     await page.pause();
     const productTitles = await pm.homePage.getProductTitles();
@@ -37,7 +36,6 @@ test('sort product by alphabatically a-z', async({ pm, page}) => {
 })
 
 test('sort product by alphabatically z-a', async({pm, page}) => {
-    await pm.homePage.gotoHomePage();
     await pm.homePage.sortBy(product.sortOptions.nameZtoA);
     const productTitles = await pm.homePage.getProductTitles();
     const sorted = [...productTitles].sort((a,b) => b.localeCompare(a));
